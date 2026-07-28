5685a840902df3cb7b1cdc9186701200a436a15d
fadynemer21 <fadynemer76@gmail.com>
2026-07-28 17:28:37 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/fadynemer/cutime/screens/SplashScreen.kt
@@ -0,0 +1,9 @@
+package com.fadynemer.cutime.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun SplashScreen() {
+    Text("Splash Screen")
+}
